1493de7fb364e67f5a9a78dc836c84474e199644
Admin <ilyassayfutdinov0087@gmail.com>
2023-07-03 23:24:55 +0300

    First upload

---
+++ WebApplication1/Controllers/FormDataController.cs
@@ -0,0 +1,57 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+
+namespace WebApplication1.Controllers
+{
+    public class FormDataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Chek(FormData formData)
+        {
+            GoogleCredential credential = GoogleCredential.FromFile("credential.json")
+            .CreateScoped(SheetsService.Scope.Spreadsheets);
+
+            var service = new SheetsService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential
+            });
+
+
+            string spreadsheetId = "17JssQGkBsCCe_Xb5sQWZ5e75qZJSNHgaT9GWV0xfO9E";
+
+            string range = "A1:C1";
+
+            var valueRange = new ValueRange
+            {
+                Values = new List<IList<object>>
+            {
+                new List<object> { formData.Name, formData.Number, formData.Comment }
+            }
+            };
+
+
+
+
+            if (ModelState.IsValid)
+            {
+
+                var request = service.Spreadsheets.Values.Append(valueRange, spreadsheetId, range);
+                request.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+                var response = request.Execute();
+            }
+
+            return Redirect("/");
+        }
+    }
+}
---
+++ WebApplication1/Models/FormData.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class FormData
+    {
+        
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public string Comment { get; set; }
+    }
+}
